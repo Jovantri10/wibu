2d474cb7b6d506861c5e0267324cd231f07ef1be
TodorokiDeku <51161901+Jovantri10@users.noreply.github.com>
2020-08-18 20:11:56 +0700

    Update to json
    Random character for mha

--- src/weebchar.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext imprort commands
-import os
-import random
-
-weebs =["Izuku Midoriya","Katuski Bakugou","Eijiro Kirishima","Mina Ashido","Mr.Aizawa","Sinsho","Todoroki Shouto","Ochako Uraraka","Tenya Lida","All Might","Endeavour","Aoyama","Denki Kaminari","Jirou Kyoya","Toga Himiko"]
-
-def char():
-    random.choice(weebs)
